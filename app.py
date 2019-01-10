bfffed0bfd1622af9b53332c1c52eb5845bc5436
Bryan Gilbert <gilbertb4375@gmail.com>
2019-01-10 16:25:36 -0600

    Convert format strings into formatted string literals

--- app.py
+++ app.py
@@ -8,10 +8,9 @@ adj5 = input("Enter an adjective: ")
 verb1 = input("Enter a verb: ")
 adj6 = input("Enter an adjective: ")
 
-story = "There is no power in the universe quite like the Force. The Force is what gives a Jedi his {} power.\n" \
-        "It's an energy field created by all {} {}. It binds the {} together. The force can be used for {} or {}.\n" \
-        "The dark side is {} and the {} side is good! The Force {} a Jedi strong.".format(adj1, adj2, noun1,
-                                                                                          noun2, adj3, adj4,
-                                                                                          adj5, verb1, adj6)
+story = f"There is no power in the universe quite like the Force. The Force is what gives a Jedi his {adj1} power.\n" \
+        f"It's an energy field created by all {adj2} {noun1}. It binds the {noun2} together. The force can be " \
+        f"used for {adj3} or {adj4}.\n" \
+        f"The dark side is {adj5} and the {adj6} side is good! The Force {verb1} a Jedi strong."
 print(story)
 
